6f8d9cfea59caeba99e1625645cd43ff2c6b09dc
meerfolk <meerfolk@gmail.com>
2021-12-11 19:52:34 +0500

    Add handled message checking

--- src/domain/message/message.service.ts
+++ src/domain/message/message.service.ts
@@ -1,19 +1,28 @@
 import { IBotService, SendMessageModel } from '../bot';
 
 export class MessageService {
+    private readonly messageIdsSet = new Set();
+
     constructor(private readonly botService: IBotService, private readonly availableChatIds: Array<number>) {}
 
     public async handleAudioMessages(): Promise<void> {
         const audioMessages = await this.botService.getAudioMessages();
 
         const filteredAudioMessages = audioMessages.filter((message) => {
-            return this.availableChatIds.includes(message.chatId);
+            return !this.messageIdsSet.has(message.messageId)
+                && this.availableChatIds.includes(message.chatId);
         });
 
-        await Promise.all(filteredAudioMessages.map((message) => {
+        await Promise.all(filteredAudioMessages.map(async (message) => {
             const sendMessage = new SendMessageModel('Message handled', message.chatId, message.messageId);
 
-            return this.botService.sendMessage(sendMessage);
+            try {
+                await this.botService.sendMessage(sendMessage);
+            } catch (err) {
+                console.error(err);
+            }
+
+            this.messageIdsSet.add(message.messageId);
         }));
     }
 }
