68777f8902228fa822e0ecfeeddb28247881df40
meerfolk <meerfolk@gmail.com>
2021-12-14 22:40:49 +0500

    Add azure storage configuration

--- configuration/default.js
+++ configuration/default.js
@@ -6,5 +6,14 @@ module.exports = {
         token: process.env.TELEGRAM_TOKEN,
     },
     availableChatIds: JSON.parse(process.env.AVAILABLE_CHAT_IDS || '[]'),
-    workerInterval: 5 * 60 * 1000,
+    workerInterval: 5 * 1000,
+    cloud: {
+        azure: {
+            storage: {
+                account: process.env.AZURE_STORAGE_ACCOUNT,
+                sas: process.env.AZURE_STORAGE_SAS,
+                container: process.env.AZURE_STORAGE_CONTAINER,
+            }
+        }
+    }
 };
