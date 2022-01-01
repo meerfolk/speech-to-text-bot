10478456e7fbca67a6232736e98297a9fc0528b9
meerfolk <meerfolk@gmail.com>
2022-01-01 23:27:30 +0500

    Use logger in presentation layer

--- src/presentation/worker.ts
+++ src/presentation/worker.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import { MessageService } from '../domain/message';
+import { ILoggerService } from '../domain/interfaces';
 import { ConfigurationService } from '../infrastructure/configuration/configuration.service';
 import { diContainer } from '../infrastructure/di-container';
 
@@ -8,6 +9,7 @@ let interval: NodeJS.Timer | null = null;
 
 const messageService = diContainer.get<MessageService>('MessageService');
 const configurationService = diContainer.get<ConfigurationService>('ConfigurationService');
+const loggerService = diContainer.get<ILoggerService>('LoggerService');
 
 (async function() {
     const intervalTime = configurationService.get<number>('workerInterval');
@@ -15,7 +17,7 @@ const configurationService = diContainer.get<ConfigurationService>('Configuratio
     interval = setInterval(() => {
         messageService.handleAudioMessages()
             .catch((error) => {
-                console.error(error);
+                loggerService.error(error);
             });
     }, intervalTime);
 })();
